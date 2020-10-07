931a2623729b74f94c91ad1105dc5157171669f5
12Aleks <tanais2010@yahoo.com>
2020-10-07 20:26:25 +0200

    added materialize icons in app

--- app/src/views/Departments.vue
+++ app/src/views/Departments.vue
@@ -1,6 +1,26 @@
 <template>
     <div>
+        <div class="app-main-layout">
+            <main class="app-content">
+                <div class="app-page">
+                    <div>
+                        <div class="page-title">
+                            <h3>Zespoły</h3>
+                        </div>
 
+                        <div class="row">
+                            <div class="col s12 m6 l4">
+
+                            </div>
+
+                            <div class="col s12 m6 l8">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
     </div>
 </template>
 
