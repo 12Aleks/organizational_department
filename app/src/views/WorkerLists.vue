53ff881ad6a519f33f74507258d7e96fa226e8b9
12Aleks <tanais2010@yahoo.com>
2020-11-25 20:58:50 +0100

    added filter for workers list

--- app/src/views/WorkerLists.vue
+++ app/src/views/WorkerLists.vue
@@ -7,28 +7,41 @@
         </div>
 
         <div class="row img_attachment">
-          <Loader v-if="loader" />
+          <Loader v-if="loader"/>
           <div v-show="!loader" class="col s12 m12">
+            <input type="text" v-model="selectWorker" placeholder="Wyszukiwanie po imieniu i nazwisku">
             <div class="table-wrapper">
               <table class="highlight">
                 <thead>
                 <tr>
-                  <th :class="{active: sortParam === 'name'}" @click="sortParam='name'">Nawisko i Imię</th>
-                  <th :class="{active: sortParam==='department'}" @click="sortParam='department'">Zespół</th>
-                  <th :class="{active: sortParam==='sections'}" @click="sortParam='sections'">Komórka</th>
-                  <th :class="{active: sortParam==='salary'}" @click="sortParam='salary'">Aktualne wynagrodzenie <br /> ([CKP]/za godzinę)</th>
-                  <th :class="{active: sortParam==='final_salary'}" @click="sortParam='final_salary'">Uzgodnione z Pracownikiem <br />([CKP]/za godzinę)
+                  <th :class="{active: sortParam==='name' && selectWorker===''}"
+                      @click="sortParam='name'; selectWorker = ''">Nawisko i Imię
+                  </th>
+                  <th :class="{active: sortParam==='department' && selectWorker===''}"
+                      @click="sortParam='department'; selectWorker = '' ">Zespół
+                  </th>
+                  <th :class="{active: sortParam==='sections'&& selectWorker===''}"
+                      @click="sortParam='sections'; selectWorker = ''">Komórka
+                  </th>
+                  <th :class="{active: sortParam==='salary'&& selectWorker === ''}"
+                      @click="sortParam='salary'; selectWorker = '' ">Aktualne wynagrodzenie <br/> ([CKP]/za godzinę)
+                  </th>
+                  <th :class="{active: sortParam==='final_salary'&& selectWorker===''}"
+                      @click="sortParam='final_salary'; selectWorker = ''">Uzgodnione z Pracownikiem <br/>([CKP]/za
+                    godzinę)
                   </th>
                 </tr>
                 </thead>
                 <tbody id="table" v-for="(value, name, index) in sortedList" :key="index">
                 <tr>
                   <td>{{ value.name }}</td>
-                  <td><router-link :to="`/department/${value.sections === '(puste)' && value.department === '(puste)' ? value.process :
+                  <td>
+                    <router-link :to="`/department/${value.sections === '(puste)' && value.department === '(puste)' ? value.process :
                           value.department}`">{{
-                      value.sections === '(puste)' && value.department === '(puste)' ? value.process :
-                          value.department
-                    }}</router-link>
+                        value.sections === '(puste)' && value.department === '(puste)' ? value.process :
+                            value.department
+                      }}
+                    </router-link>
                   </td>
                   <td>{{ value.sections !== '(puste)' ? value.sections : '(puste)' }}</td>
                   <td>{{ value.salary }}zł / {{ value.per_hour }}zł/god.</td>
@@ -48,35 +61,42 @@
 
 export default {
   name: "WorkerLists",
-  data(){
+  data() {
     return {
+      selectWorker: '',
       sortParam: '',
       loader: true,
       workersInfo: [],
-      worker: ''
+      defaultWorkersInfo: []
     }
   },
   async mounted() {
-    this.workersInfo = await this.$store.dispatch('receiveData');
+    const defaultWorkersInfo = await this.$store.dispatch('receiveData');
+    this.defaultWorkersInfo = defaultWorkersInfo;
+    this.workersInfo = defaultWorkersInfo;
     this.loader = false;
   },
   computed: {
     sortedList() {
-
-        switch (this.sortParam) {
-          case 'name':
-            return this.workersInfo.sort((d1, d2) => d1.name.toLowerCase() > d2.name.toLowerCase() ? 1 : -1);
-          case 'department':
-            return this.workersInfo.sort((d1, d2) => d1.department.toLowerCase() > d2.department.toLowerCase() ? 1 : -1);
-          case 'sections':
-            return this.workersInfo.sort((d1, d2) => d1.sections.toLowerCase() > d2.sections.toLowerCase() ? 1 : -1);
-          case 'salary':
-            return this.workersInfo.sort((d1, d2) => d1.salary > d2.salary ? 1 : -1);
-          case 'final_salary':
-            return this.workersInfo.sort((d1, d2) => d1.final_salary > d2.final_salary ? 1 : -1);
-          default:
-            return this.workersInfo;
-        }
+      if (this.sortParam === 'name' && !this.selectWorker.length) {
+        return this.workersInfo.sort((d1, d2) => d1.name.toLowerCase() > d2.name.toLowerCase() ? 1 : -1);
+      } else if (this.sortParam === 'department' && !this.selectWorker.length) {
+        return this.workersInfo.sort((d1, d2) => d1.department.toLowerCase() > d2.department.toLowerCase() ? 1 : -1);
+      } else if (this.sortParam === 'sections' && !this.selectWorker.length) {
+        return this.workersInfo.sort((d1, d2) => d1.sections.toLowerCase() > d2.sections.toLowerCase() ? 1 : -1);
+      } else if (this.sortParam === 'salary' && !this.selectWorker.length) {
+        return this.workersInfo.sort((d1, d2) => d1.salary > d2.salary ? 1 : -1);
+      } else if (this.sortParam === 'final_salary' && !this.selectWorker.length) {
+        return this.workersInfo.sort((d1, d2) => d1.final_salary > d2.final_salary ? 1 : -1);
+      } else if (this.selectWorker.length) {
+        let selectWorker = this.selectWorker.toUpperCase();
+        return this.defaultWorkersInfo.filter(function (elem) {
+          if (selectWorker === ' ') return true;
+          else return elem.name.indexOf(selectWorker) > -1;
+        })
+      } else {
+        return this.workersInfo;
+      }
     }
   }
 }
@@ -84,10 +104,27 @@ export default {
 
 <style scoped>
 .table-wrapper {
-  height: -webkit-calc(100vh - 225px);
-  height: calc(100vh - 225px);
+  height: -webkit-calc(100vh - 290px);
+  height: calc(100vh - 290px);
 }
-
+input[type='text']{
+ margin-bottom: 15px;
+}
+::-webkit-input-placeholder { /* WebKit browsers */
+  text-transform: uppercase;
+}
+:-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+  text-transform: uppercase;
+}
+::-moz-placeholder { /* Mozilla Firefox 19+ */
+  text-transform: uppercase;
+}
+:-ms-input-placeholder { /* Internet Explorer 10+ */
+  text-transform: uppercase;
+}
+::placeholder { /* Recent browsers */
+  text-transform: uppercase;
+}
 table {
   table-layout: fixed;
 }
