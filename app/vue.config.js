39218415c36f682c8b8235131766881b25ea8c94
12Aleks <tanais2010@yahoo.com>
2021-02-20 14:41:44 +0100

    changed vue.config.js file (changed manifest options in file)

--- app/vue.config.js
+++ app/vue.config.js
@@ -7,16 +7,43 @@ module.exports = {
         }
     },
     pwa: {
-        name: 'Terma departments',
-        themeColor: 'rgba(255, 104, 115, 1)',
-        msTileColor: '#000000',
-        appleMobileWebAppCapable: 'yes',
-        appleMobileWebAppStatusBarStyle: 'black',
-        iconPaths: {
-            favicon32: './img/icons/android-chrome-36x36.png',
-            favicon16: './img/icons/favicon16x16.png',
-            appleTouchIcon: './img/icons/apple-icon-57x57.png',
-            msTileImage: 'img/icons/ms-icon-144x144.png'
+        manifestOptions: {
+            name: "Terma departments",
+            short_name: "Terma",
+            start_url: "./",
+            display: "standalone",
+            theme_color: "#000000",
+            icons: [
+                {
+                    'src': 'img/icons/chrome-72x72.png',
+                    'sizes': '72x72',
+                    'type': 'image/png',
+                },
+                {
+                    src: "img/icons/chrome192x192.png",
+                    sizes: "192x192",
+                    type: "image/png",
+                },
+                {
+                    'src': 'img/icons/chrome-48x48.png',
+                    'sizes': '48x48',
+                    'type': 'image/png',
+                },
+            ],
         },
-    }
+
+        themeColor: "rgba(242, 241, 239, 1)",
+        msTileColor: "#000000",
+        appleMobileWebAppCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "black",
+        iconPaths: {
+            favicon32: "img/icons/chrome-32x32.png",
+            favicon16: "img/icons/chrome-16x16.png",
+            maskIcon: "img/icons/svg.svg",
+            appleTouchIcon: "img/icons/chrome192x192.png",
+            msTileImage: "img/icons/mstile-144x144.png",
+        },
+        // configure the workbox plugin
+        workboxPluginMode: "GenerateSW",
+    },
 }
