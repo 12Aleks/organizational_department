f8e7daa659ff12bc87400cbf9362287d8047ec84
12Aleks <tanais2010@yahoo.com>
2021-02-20 11:19:24 +0100

    added manifest file and change vue.config.js file, added pwa

--- app/vue.config.js
+++ app/vue.config.js
@@ -5,5 +5,18 @@ module.exports = {
                 additionalData: `@import "~@/styles/styles.scss";`
             },
         }
+    },
+    pwa: {
+        name: 'Terma departments',
+        themeColor: 'rgba(255, 104, 115, 1)',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        iconPaths: {
+            favicon32: './img/icons/android-chrome-36x36.png',
+            favicon16: './img/icons/favicon16x16.png',
+            appleTouchIcon: './img/icons/apple-icon-57x57.png',
+            msTileImage: 'img/icons/ms-icon-144x144.png'
+        },
     }
 }
